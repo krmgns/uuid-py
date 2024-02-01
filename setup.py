d8d85fa8f0326862383d7d74bdc55a5a24545b41
Kerem Güneş <1508306+krmgns@users.noreply.github.com>
2024-02-01 05:41:22 +0300

    First commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2023 · Kerem Güneş
+# Apache License 2.0 · https://github.com/krmgns/uuid
+
+from setuptools import setup
+
+setup(
+   name            = 'ouuid',
+   description     = 'Random or sortable UUIDs with date/time prefixes.',
+   license         = 'Apache License 2.0',
+   author          = 'Kerem Güneş',
+   keywords        = ['uuid'],
+   url             = 'https://github.com/krmgns/uuid-py',
+   version         = '1.0.0',
+   python_requires = '>3.9.0',
+)
